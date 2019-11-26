d60d6b5e2e9ea5bb232d3b09fd23dc8f8712e726
allankof <54761510+allankof@users.noreply.github.com>
2019-11-26 19:20:10 +0800

    鏡頭跟隨

--- KingdomRevival/Assets/scripts/Elf.cs
+++ KingdomRevival/Assets/scripts/Elf.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class Elf : MonoBehaviour
 {
+
+    private float moveSpeed = 10f;
+
+    public Transform mainCemaraPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCemaraPosition = GameObject.Find("Main Camera").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveTo();
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        //mainCemaraPosition.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+        
     }
 
     private void MoveTo()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-10 * Time.deltaTime, 0, 0);
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(-10 * Time.deltaTime, 0, 0);
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
     }
---
+++ KingdomRevival/Assets/scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class GameManager : MonoBehaviour
+{
+    Transform player, mainCamera;
+    Vector3 offset;
+
+    public GameObject bornPoint;
+    public Tile[] GetTile;
+    public Tilemap myTilemap;
+
+    private Vector3Int bornLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TilemapBuild();
+
+        player = GameObject.Find("精靈王").transform;
+        mainCamera = GameObject.Find("Main Camera").transform;
+        offset = player.position - mainCamera.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.position = player.position - offset;
+
+    }
+
+    private void TilemapBuild()
+    {
+        bornLocation = myTilemap.WorldToCell(transform.position);
+        myTilemap.SetTile(bornLocation, GetTile[0]);
+
+        for (int i = 0; i < 10; i++)
+        {
+            bornLocation.x += 5;
+            myTilemap.SetTile(bornLocation, GetTile[0]);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            bornLocation.x += 9;
+            myTilemap.SetTile(bornLocation, GetTile[1]);
+        }
+
+    }
+
+
+}
