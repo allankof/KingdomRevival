edb16863d6de0cf5a7b4291c92d5f39b79ad8ef6
allankof <54761510+allankof@users.noreply.github.com>
2019-11-30 17:48:35 +0800

    角色預置物

--- KingdomRevival/Assets/scripts/Elf.cs
+++ KingdomRevival/Assets/scripts/Elf.cs
@@ -4,43 +4,56 @@ using UnityEngine;
 
 public class Elf : MonoBehaviour
 {
+    public GameObject player;
 
-    private float moveSpeed = 10f;
+    private float moveSpeed = 10.0f;
+    private float thrust = 1.0f;
 
-    public Transform mainCemaraPosition;
+    private Animator ani;
+    private Rigidbody2D rb2;
 
     // Start is called before the first frame update
     void Start()
     {
-        mainCemaraPosition = GameObject.Find("Main Camera").transform;
+        ani = GetComponent<Animator>();
+        rb2 = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveTo();
-        MoveCamera();
-    }
-
-    private void MoveCamera()
-    {
-        //mainCemaraPosition.Translate(-moveSpeed * Time.deltaTime, 0, 0);
         
     }
 
+    /// <summary>
+    /// 玩家移動
+    /// </summary>
     private void MoveTo()
     {
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            ani.SetBool("走路開關", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
             transform.rotation = Quaternion.Euler(0, 180, 0);
+            ani.SetBool("走路開關", true);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2.isKinematic = false;
+            rb2.AddForce(transform.right * thrust, ForceMode2D.Impulse);
+            rb2.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+
+            //rb2.AddRelativeForce(transform.position, ForceMode2D.Impulse);
         }
     }
+
 
     private void Buildings()
     {
--- KingdomRevival/Assets/scripts/GameManager.cs
+++ KingdomRevival/Assets/scripts/GameManager.cs
@@ -6,24 +6,26 @@ using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
+    // 玩家與相機座標
     Transform player, mainCamera;
+    // 座標距離
     Vector3 offset;
 
+    [Header("植物生成點")]
     public GameObject bornPoint;
+    [Header("要使用的圖塊")]
     public Tile[] GetTile;
+    [Header("目標Tilemap")]
     public Tilemap myTilemap;
-
-    private Vector3Int bornLocation;
 
     // Start is called before the first frame update
     void Start()
     {
-        TilemapBuild();
-
         player = GameObject.Find("精靈王").transform;
         mainCamera = GameObject.Find("Main Camera").transform;
         offset = player.position - mainCamera.position;
-
+        
+        TilemapBorn();
     }
 
     // Update is called once per frame
@@ -33,23 +35,67 @@ public class GameManager : MonoBehaviour
 
     }
 
-    private void TilemapBuild()
+    /// <summary>
+    /// 生成背景植物
+    /// </summary>
+    private void TilemapBorn()
     {
-        bornLocation = myTilemap.WorldToCell(transform.position);
-        myTilemap.SetTile(bornLocation, GetTile[0]);
+        Vector3Int bornLocation = myTilemap.WorldToCell(bornPoint.transform.position);
+        
+        List<Tile> myTiles = new List<Tile>();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < GetTile.Length; i++)
         {
-            bornLocation.x += 5;
-            myTilemap.SetTile(bornLocation, GetTile[0]);
+            myTiles.Add(GetTile[i]);
         }
-        for (int i = 0; i < 10; i++)
-        {
-            bornLocation.x += 9;
-            myTilemap.SetTile(bornLocation, GetTile[1]);
-        }
-
+        
+        StartCoroutine(AddTilemap(myTilemap, bornLocation, myTiles));
     }
 
+    /// <summary>
+    /// 協同程序
+    /// </summary>
+    /// <param name="tilemap"></param>
+    /// <param name="position"></param>
+    /// <param name="tiles"></param>
+    /// <returns></returns>
+    private IEnumerator AddTilemap(Tilemap tilemap, Vector3Int position, List<Tile> tiles)
+    {
+        TreeRandom(tilemap, position, tiles);
+        do
+        {
+            position.x += Random.Range(2, 6);
+            TreeRandom(tilemap, position, tiles);
+            position.x += Random.Range(2, 12);
+            //Debug.Log(position.x);
+        } while (position.x < 120);
+
+            yield return new WaitForSeconds(0.2f);
+    }
+
+    /// <summary>
+    /// 隨機選擇植物
+    /// </summary>
+    /// <param name="tilemap"></param>
+    /// <param name="position"></param>
+    /// <param name="tiles"></param>
+    private void TreeRandom(Tilemap tilemap, Vector3Int position, List<Tile> tiles)
+    {
+        int n = Random.Range(0, 3);
+        if (n == 0)
+        {
+            position.y -= 3;
+            tilemap.SetTile(position, tiles[n]);
+        }
+        else if (n == 1)
+        {
+            tilemap.SetTile(position, tiles[n]);
+        }
+        else
+        {
+            position.y -= 4;
+            tilemap.SetTile(position, tiles[n]);
+        }
+    }
 
 }
